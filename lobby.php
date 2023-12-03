3919882d44ac11648dbee9aa705ddd3ea0e87cfb
PracticallyNothing <practicallynothingatall@gmail.com>
2023-12-03 20:43:23 +0200

    Add logout + automatic redirect for logged in user

--- lobby.php
+++ lobby.php
@@ -1,14 +1,36 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["USERNAME"])) {
+    header("Location: index.php");
+    return;
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/htmx/1.9.9/htmx.min.js" integrity="sha512-FSS62yxqCRMCtm1J+ddRwX8DuCRVt/WMpihCo06P+Je5AG4CV9yoLX53zHaOB5w/eZdG7d/QAyUEJTnHZHrWKg==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://unpkg.com/htmx.org/dist/ext/debug.js"></script>
+
     <title>Robot Wars - Lobby</title>
 </head>
 
-<body>
-    <h1> WAAAAA A</h1>
+<body class="h-screen flex flex-col">
+    <div class="flex flex-row">
+        <h1 class="text-5xl font-semibold text-center mb-4 flex-grow">
+            Robot Wars Lobby
+        </h1>
+        <button hx-post="/user/do_logout.php">
+            Log Out
+        </button>
+    </div>
+
+    <hr class="w-full" />
 </body>
 
 </html>
---
+++ user/do_logout.php
@@ -0,0 +1,9 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] != "POST") {
+    http_response_code(405);
+    exit(-1);
+}
+session_start();
+session_unset();
+
+header("HX-Redirect: index.php");
