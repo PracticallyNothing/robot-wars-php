a09866e53d00e17cefafd15d94f7ea6f8420c1b5
PracticallyNothing <practicallynothingatall@gmail.com>
2023-12-03 17:49:46 +0200

    do_register.php - Minor formatting

--- user/do_register.php
+++ user/do_register.php
@@ -7,27 +7,22 @@ if ($_SERVER["REQUEST_METHOD"] != "POST") {
 function show_register_error(string $error_text)
 {
     echo '<p class="col-span-2 mt-2 text-red-700 text-center" id="register-error" hx-swap-oob="true">' . $error_text . '</p>';
-}
-
-if (!isset($_POST["register-submit"])) {
-    show_register_error("Incorrect format!");
     exit(-1);
 }
+
+if (!isset($_POST["register-submit"]))
+    show_register_error("Incorrect format!");
 
 $username = trim((string)$_POST["username"]);
 $password = (string)$_POST["password"];
 $email = trim((string)$_POST["email"]);
 
-if (strlen($username) == 0) {
+if (strlen($username) == 0)
     show_register_error("Username must not be empty!");
-    exit(-1);
-} else if (strlen($password) == 0) {
+else if (strlen($password) == 0)
     show_register_error("Password must not be empty!");
-    exit(-1);
-} else if (strlen($email) == 0) {
+else if (strlen($email) == 0)
     show_register_error("Email must not be empty!");
-    exit(-1);
-}
 
 include_once "../connection.php";
 
