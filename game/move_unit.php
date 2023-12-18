50b6e59ae3777a2ea87f32e15c30ac01109b287b
PracticallyNothing <practicallynothingatall@gmail.com>
2023-12-19 00:32:50 +0200

    Refactor move_unit.php

--- game/move_unit.php
+++ game/move_unit.php
@@ -9,6 +9,13 @@ $gameid = $_SESSION["gameid"];
 $unitId = (int)$_POST["unitId"];
 $sector = (string)$_POST["sector"];
 
+// TODO(Марио):
+//   Въобще не е хубаво, че тези неща са хардкоднати!
+define("NUM_SECTORS", 8);
+define("MAX_SECTOR", ord("A") + NUM_SECTORS);
+define("NUM_RADIUSES", 9);
+
+
 $now = new DateTimeImmutable(exec_sql_scalar($conn, "select current_timestamp"));
 $now_str = $now->format(DT_FORMAT);
 
@@ -18,17 +25,12 @@ $unit_speed = exec_sql_scalar(
     [$unitId]
 );
 
-sector_to_xy($sector);
-
 function sector_to_xy(string $sectorName)
 {
     // Use letter to determine angle
-    define("NUM_SECTORS", ord("H"));
-    define("NUM_RADIUSES", 9);
-
     $angleRad = M_PI * 2 * (
-        (ord($sectorName[0]) - ord("A")) / (NUM_SECTORS + 1 - ord("A"))
-    ) + deg2rad(360 / 16);
+        (ord($sectorName[0]) - ord("A")) / (MAX_SECTOR - ord("A"))
+    ) + deg2rad(360 / NUM_SECTORS / 2);
 
     // Use number to determine distance
     $dist = 0.1 + 0.9 * (((float)$sectorName[1] - 0.5) / NUM_RADIUSES);
@@ -36,11 +38,10 @@ function sector_to_xy(string $sectorName)
     $x = cos($angleRad) * $dist;
     $y = sin($angleRad) * $dist;
 
-    header("Content-Type: application/json");
-    echo json_encode(["angleRad" => $angleRad, "x" => $x, "y" => $y,]);
+    return ["x" => $x, "y" => $y];
 }
 
-function calc_unit_position($conn, $unitId, $gameid)
+function calc_unit_curr_position($conn, $unitId, $gameid)
 {
     $last_move_command = exec_sql_first($conn, "
       select UnitStartXPos as X, UnitStartYPos as Y, Sector
@@ -58,3 +59,7 @@ function calc_unit_position($conn, $unitId, $gameid)
 
     return ["x" => 1, "y" => 1];
 }
+
+
+header("Content-Type: application/json");
+echo json_encode(sector_to_xy($sector));
